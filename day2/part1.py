5e7911d5e848d8cba442b9d201a16497384f0738
Alexander Karlsson <alexander.karlsson@holmgrensbil.se>
2019-12-19 15:39:45 +0100

    Day 2 finish

--- day2/part1.py
+++ day2/part1.py
@@ -3,20 +3,26 @@ def chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
-test = list(map(int, open('day2/test.txt','r').read().split(',')))
+test = list(map(int, open('day2/input.txt','r').read().split(',')))
+#copyx = test.copy()
 
 for x in range(0, len(test), 4):
-    var0 = test[x]
-    var1 = test[x+1]
-    var2 = test[x+2]
-    var3 = test[x+3]
+    if x < len(test) and test[x] != 99:
 
-    print(str(var0) + ' | ' + str(var1) + ' | ' + str(var2) + ' | ' + str(var3))
+        var0 = test[x]
+        var1 = test[x+1]
+        var2 = test[x+2]
+        var3 = test[x+3]
 
-    if var0 == 1:
-        print('1')
-    elif var0 == 99:
-        break;
+        #print(str(var0) + ' | ' + str(var1) + ' | ' + str(var2) + ' | ' + str(var3))
+        if var0 == 1:
+            test[var3] = test[var1] + test[var2]
+        elif var0 == 2:
+            test[var3] = test[var1] * test[var2]
+        elif var0 == 99:
+            break
+    else:
+        break
 print(test)
 
 # modules = list(map(int, open('day2/input.txt','r').read()))
